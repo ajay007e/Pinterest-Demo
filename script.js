5c99bdf686eb24a5ad87c6f26ca30c2a0fbedcf5
Ajay Mathew <ajayedathazhe@gmail.com>
2022-01-18 16:00:18 +0530

    add new features

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const clientHeight = document.documentElement.clientHeight;
 const clientWidth = document.documentElement.clientWidth;
 const vids = document.querySelectorAll("video");
 
+let wait = true;
+
 window.onload = () => {
   document.querySelector(".buttons").style.transform = "translate(0,0)";
 };
@@ -26,7 +28,7 @@ document.addEventListener("scroll", () => {
       i.getBoundingClientRect().top > 0 &&
       i.getBoundingClientRect().bottom > 0
     ) {
-      console.log(i);
+      // console.log(i);
       i.play();
       i.parentElement.children[0].style.opacity = "0";
       flag = false;
@@ -36,4 +38,60 @@ document.addEventListener("scroll", () => {
     }
     // console.log(i.outerHTML);
   });
+
+  if (
+    wait &&
+    Number(
+      (
+        (window.innerHeight + window.scrollY) /
+        document.documentElement.scrollHeight
+      ).toFixed(2)
+    ) === 0.99
+  ) {
+    wait = false;
+    document.querySelector(".loader_section").classList.add("active");
+    setTimeout(addContent, 2000);
+  }
 });
+
+for (let i = 0; i < vids.length; i++) {
+  vids[i].addEventListener("mouseenter", () => {
+    vids[i].muted = false;
+  });
+  vids[i].addEventListener("mouseout", () => {
+    vids[i].muted = true;
+  });
+}
+
+const addContent = () => {
+  console.log("bottom");
+  const cards = [
+    "card_x-small",
+    "card_small",
+    "card_medium",
+    "card_large",
+    "card_x-large",
+  ];
+  const cardCount = getRandomInt(10, 30);
+  let finalStr = "";
+  for (let i = 0; i < cardCount; i++) {
+    let cardSize = cards[getRandomInt(0, 5)];
+    let isAlbum = getRandomInt(0, 6) === getRandomInt(0, 6) ? true : false;
+    let isVideo = getRandomInt(0, 6) === getRandomInt(0, 6) ? true : false;
+    let str = `<div class="card ${cardSize} skeleton">
+    <div class="img _skeleton"></div>
+  </div>`;
+    finalStr += str;
+    // console.log(cardCount, cardSize, isAlbum,isVideo);
+  }
+  document.querySelector(".pin_container").innerHTML += finalStr;
+  document.querySelector(".loader_section").classList.remove("active");
+
+  wait = true;
+};
+
+const getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+};
